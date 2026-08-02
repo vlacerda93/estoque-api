86a3f2d9b40c13dd315962d68a9b7841ceb30e26
vlacerda93 <vinicius.lacerda@example.com>
2026-08-02 18:20:56 -0300

    fix: adiciona rota de status GET

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const produtoRoutes = require('./routes/produtoRoutes');
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => res.json({ status: 'API de Estoque rodando com sucesso!' }));
+app.get('/api/status', (req, res) => res.json({ status: 'API de Estoque rodando com sucesso!' }));
+
 app.use('/api', produtoRoutes);
 
 app.listen(3000, () => console.log('API rodando na porta 3000'));
